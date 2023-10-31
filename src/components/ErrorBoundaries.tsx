b25ff886fd9793d89cbfd45900536310a7c33cf3
lehuutai0310 <lehuutai0310@gmail.com>
2023-10-31 16:16:43 +0700

    Feature: add src exxample for error boundaries

---
+++ src/components/ErrorBoundaries.tsx
@@ -0,0 +1,34 @@
+import React, { Component, ReactNode } from "react";
+
+interface ErrorBoundaryProps {
+  fallback: ReactNode;
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo): void {
+    console.log("Error:", error);
+    console.log("Error Info:", info);
+    this.setState({ hasError: true });
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
