ec46201afecc532a84b19772316a3b0059039570
lehuutai0310 <lehuutai0310@gmail.com>
2023-11-01 16:45:44 +0700

    Feature: add src code demo for profiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react";
+import { Profiler, Suspense, lazy } from "react";
 import "./App.css";
 import ListProduct from "./components/ListProduct";
 import { delayForDemo } from "./helpers/DelayDemo";
@@ -8,11 +8,30 @@ import FormInput from "./components/FormInput";
 const Content = lazy(() => delayForDemo(import("./components/Content")));
 
 function App() {
+  const onRender = (
+    id: string,
+    phase: string,
+    actualTime: number,
+    baseTime: number,
+    startTime: number,
+    commitTime: number
+  ) => {
+    console.log(`${id}'s ${phase} phase:`);
+    console.log(`Actual time: ${actualTime}`);
+    console.log(`Base time: ${baseTime}`);
+    console.log(`Start time: ${startTime}`);
+    console.log(`Commit time: ${commitTime}`);
+  };
+
   return (
     <>
       <Suspense fallback={<p>Loading...</p>}>
-        <Content />
-        <ListProduct />
+        <Profiler id="Content" onRender={onRender}>
+          <Content />
+        </Profiler>
+        <Profiler id="ListProduct" onRender={onRender}>
+          <ListProduct />
+        </Profiler>
         <Input />
       </Suspense>
       <FormInput />
